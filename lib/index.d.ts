7bd348940177ad2987f78da42f41540e99044c6d
TJRobson <thomas.j.robson@hotmail.com>
2017-02-03 00:48:21 +1300

    wow what a lot of set up

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,2 @@
 import { Greeter } from './greeter';
-
 export { Greeter };
---
+++ test/greeter-test.ts
@@ -0,0 +1,12 @@
+
+import * as chai from 'chai';
+import { Greeter } from '../src/greeter';
+
+const expect = chai.expect;
+
+describe('greeter', () => {
+  it('should greet with message', () => {
+    const greeter = new Greeter('friend');
+    expect(greeter.greet()).to.equal('Bonjour, friend!');
+  });
+});
